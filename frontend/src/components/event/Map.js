aef7a7cdfb46e83b75f4c5c1e39775d8298c38f5
Benga <benga.olasebikan@gmail.com>
2020-09-19 01:12:27 +0100

    interactive map added

--- frontend/src/components/event/Map.js
+++ frontend/src/components/event/Map.js
@@ -4,26 +4,56 @@ import MapGl, { Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
-const Map = ({ longitude, latitude }) => (
+// const Map = ({ longitude, latitude }) => (
 
-  //* currently using personal access token, must get own before deployment
-    <MapGl
+//   //* currently using personal access token, must get own before deployment
+//     <MapGl
+//       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+//       height={'280px'}
+//       width={'320px'}
+//       mapStyle='mapbox://styles/mapbox/streets-v11'
+//       latitude={latitude}
+//       longitude={longitude}
+//       zoom={13}
+//     >
+//       <Marker
+//         latitude={latitude}
+//         longitude={longitude}
+//       >
+//         <span role="img" aria-label="marker" style={{ fontSize: '33px' }}>📍</span>
+//       </Marker>
+//     </MapGl>
+// )
+
+class Map extends React.Component {
+  state = {
+    viewport: {
+      longitude: this.props.longitude,
+      latitude: this.props.latitude,
+      height: '280px',
+      width: '320px',
+      zoom: 13
+    }
+  }
+  render() {
+    const {viewport} = this.state
+    return (
+          <MapGl
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-      height={'280px'}
-      width={'320px'}
+      {...viewport}
       mapStyle='mapbox://styles/mapbox/streets-v11'
-      latitude={latitude}
-      longitude={longitude}
-      zoom={13}
+      onViewportChange={(viewport) => {this.setState({viewport})}}
     >
       <Marker
-        latitude={latitude}
-        longitude={longitude}
+        latitude={viewport.latitude}
+        longitude={viewport.longitude}
       >
         <span role="img" aria-label="marker" style={{ fontSize: '33px' }}>📍</span>
       </Marker>
     </MapGl>
-)
+    )
+  }
+}
 
 
 
